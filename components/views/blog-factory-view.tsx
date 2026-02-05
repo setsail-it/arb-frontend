98ab2a4d735b5bc1d68b3eec38edd344c2b4fc69
Eric <ericlaycock44@gmail.com>
2026-02-05 14:33:58 -0800

    Add Textarea import for bulk keyword paste feature

--- components/views/blog-factory-view.tsx
+++ components/views/blog-factory-view.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Spinner } from "@/components/ui/spinner"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import {
